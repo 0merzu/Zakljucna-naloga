e2c549abf4f904ae2a260805f6d3804ed1f7e21e
0merzu <oskar.omerzu@gmail.com>
2025-05-05 20:47:00 +0200

    Update app.py - /host_dashboard

--- Carmeet/app.py
+++ Carmeet/app.py
@@ -59,6 +59,63 @@ def login():
 
     return render_template('login.html')
 
+@app.route('/host_dashboard', methods=['GET', 'POST'])
+def host_dashboard():
+    email = session.get('email')
+    
+
+    if request.method == 'POST':
+        IG_handle = request.form['IG_handle']
+        znamka = request.form['znamka']
+        model = request.form['model']
+        letnik = request.form['letnik']
+        engine = request.form['engine']
+        power = request.form['power']
+
+        if IG_handle[0] == "@":
+            db.update({'IG_handle': IG_handle}, User.email == email)
+        else:
+            IG_handle = "@" + IG_handle
+            db.update({'IG_handle': IG_handle}, User.email == email)
+        
+        if db.search(User.email == email):
+            db.update({'znamka': znamka, 'model': model, 'letnik': letnik, 'engine': engine, 'power': power}, User.email == email)
+
+
+    host_count = len(db.search(User.role == 'host'))
+    vote_count = len(db.search(User.location.exists()))                    
+
+    locations = [u['location'] for u in db.all() if 'location' in u]
+
+    if locations:
+        top_location = max(set(locations), key=locations.count)
+        location_final = top_location
+    else:
+        location_final = "Glasovanje še ni zaključeno"  
+
+    locations = {
+        'location-1': [46.1068177, 14.4466918],
+        'location-2': [46.0569465, 14.5057515],
+        'location-3': [46.0510833, 14.5060582],
+        'location-4': [46.065944, 14.520100],
+    }
+
+    for key, coords in locations.items():
+        if key == top_location:                                         
+            m = folium.Map(location=coords, zoom_start=13, width='600px', height='400px')
+            folium.Marker(coords, popup=f"Lokacija: {top_location}").add_to(m)
+            loc_map = m._repr_html_()
+
+    session['loc_map'] = loc_map
+
+    return render_template(
+        'host_dashboard.html',
+        host_count=host_count,
+        vote_count=vote_count,
+        location_final=location_final,
+        loc_map = loc_map
+    )
+
 
 
 if __name__ == '__main__':
